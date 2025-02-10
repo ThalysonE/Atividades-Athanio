cb821ec6719b5cec413e208b35d9bc83a5809df1
ThalysonE <Thalysonelione8@gmail.com>
2025-02-10 12:31:55 -0300

    votacao

--- AtvidadesPHP/atividade14/votacao.php
+++ AtvidadesPHP/atividade14/votacao.php
@@ -30,40 +30,92 @@
 <body>
     <?php
         session_start();
-        $arquivo;
-        if(isset(($_SESSION["votos"]))){
-            $_SESSION["   "]
+        $quantVotosFile = "quantVotos.txt";
+        $votosDathFile = "votosDath.txt";
+        $votosHomerFile = "votosHomer.txt";
+        $votosBrancoFile = "votosBranco.txt";
+        $votosNuloFile = "votosNulo.txt";
+
+        if(!file_exists($quantVotosFile)){
+            file_put_contents($quantVotosFile,'0');
+            file_put_contents($votosDathFile,'0');
+            file_put_contents($votosBrancoFile,'0');
+            file_put_contents($votosNuloFile,'0');
+            file_put_contents($votosHomerFile,'0');
+        }
+
+        if(isset($_POST["bt-dath"])|| isset($_POST["bt-homer"])|| isset($_POST["bt-branco"]) ||isset($_POST["bt-nulo"])){
+            function atualizarVotos($arquivo) {
+                $votosR = fopen($arquivo, "r");
+                $quant = intval(fgets($votosR));
+                fclose($votosR);
+                $quant += 1;
+                file_put_contents($arquivo, $quant);
+            }
+    
+            if (isset($_POST["bt-dath"])) {
+                atualizarVotos($votosDathFile);
+            }
+            if (isset($_POST["bt-homer"])) {
+                atualizarVotos($votosHomerFile);
+            }
+            if (isset($_POST["bt-branco"])) {
+                atualizarVotos($votosBrancoFile);
+            }
+            if (isset($_POST["bt-nulo"])) {
+                atualizarVotos($votosNuloFile);
+            }
+            atualizarVotos($quantVotosFile);
         }
 
     ?>
     <h1>Sistema de Votação - IFPI</h1>
     <div class="main">
-        <b>Qual o candidato voce esccolhe para o presidente do Grêmio Estudantil?</b>
-        <div>
-            <div class="principal flex ">
-                <div class="flex separacao">
-                    <img src="darth.jpg" alt="">
-                    <p><b>Dark Vander</b> - Candidato 01</p>
-                </div>
-                <div class="flex separacao">
-                    <img src="homer.png" alt="">
-                    <p><b>Homer Simpson</b> - Candidato 02</p>
-                </div>
-            </div>
-        </div>
-        <br>
-        <br>
-        <div class="flex">
-            <button> Dath Vader</button>
-            <button>Homer Simpson</button>
-            <button>Branco</button>
-            <button>Nulo</button>
-        </div>
+        <form action="" method="POST">
+         <b>Qual o candidato voce esccolhe para o presidente do Grêmio Estudantil?</b>
+         <div>
+             <div class="principal flex ">
+                 <div class="flex separacao">
+                     <img src="darth.jpg" alt="">
+                     <p><b>Dark Vander</b> - Candidato 01</p>
+                 </div>
+                 <div class="flex separacao">
+                     <img src="homer.png" alt="">
+                     <p><b>Homer Simpson</b> - Candidato 02</p>
+                 </div>
+             </div>
+         </div>
+         <br>
+         <br>
+         <div class="flex">
+             <button name="bt-dath">Dath Vader</button>
+             <button name ="bt-homer">Homer Simpson</button>
+             <button name="bt-branco">Branco</button>
+             <button name="bt-nulo">Nulo</button>
+         </div>
+        </form>
         <fieldset>
             <legend>Resultado Parcial</legend>
             <?php
-            echo "Candidato 01 - ".$python."%";
-            echo "<div style='width:".($*5)."px; background-color: red; height: 20px;'></div>";
+                if(isset($_POST["bt-dath"])|| isset($_POST["bt-homer"])|| isset($_POST["bt-branco"]) ||isset($_POST["bt-nulo"])){
+                    $quantVotosR = fopen("quantVotos.txt", "r");
+                    $votosDathR = fopen("votosDath.txt", "r");
+                    $votosHomerR = fopen("votosHomer.txt", "r");
+                    $votosBrancoR = fopen("votosBranco.txt", "r");
+                    $votosNuloR = fopen("votosNulo.txt", "r");
+                    $votosTotais = intval(fgets($quantVotosR));
+                    $votosTotaisBranco = intval(fgets($votosBrancoR));
+                    $votosTotaisDath = intval(fgets($votosDathR));
+                    $votosTotaisHomer = intval(fgets($votosHomerR));
+                    $votosTotaisNulo = intval(fgets($votosNuloR));
+
+                    print($votosTotais);
+                    print($votosTotaisDath);
+                    echo "<li>Candidato 1 - ".floor($votosTotaisDath/$votosTotais*100)."%</li>";
+                    echo "<li>Candidato 2 - ".floor($votosTotaisHomer/$votosTotais*100)."%</li>";
+                    echo "<li>Branco - ".floor($votosTotaisBranco/$votosTotais*100)."%</li>";
+                    echo "<li>Nulo - ".floor($votosTotaisNulo/$votosTotais*100)."%</li>";
+                }
             ?>
         </fieldset>
     </div>
